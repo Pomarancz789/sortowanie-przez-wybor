c3bc31880aae46aa1295a1b0c99ec50115ba9ace
Pomarancz789 <granat-02krawcy@icloud.com>
2021-12-15 10:55:30 +0100

    popełniam zadania z lekcji

--- sortowanie-przez-wybor/main.cpp
+++ sortowanie-przez-wybor/main.cpp
@@ -1,14 +1,39 @@
-//
-//  main.cpp
-//  sortowanie-przez-wybor
-//
-//  Created by Jakub Wtorkiewicz on 15/12/2021.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void zamiana(int &x, int &y)
+{
+    int temp=0;
+    
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+void wypisywanie_tablicy(int tab[], int n)
+{
+    for (int z=0; z<n; z++)
+    {
+        cout << tab[z] <<endl;
+    }
+}
+
+int main() {
+    int tab[7]={6,4,7,8,2,0,1};
+    
+    for (int i=0; i<7; i++) {
+        int min=99999999;
+        int indexmin=0;
+        for (int j=i; j<7; j++) {
+            if (tab[j]<min) {
+                min=tab[j];
+                indexmin=j;
+            }
+        }
+        zamiana(tab[i], tab[indexmin]);
+    }
+    
+    wypisywanie_tablicy(tab, 7);
+    
     return 0;
 }
