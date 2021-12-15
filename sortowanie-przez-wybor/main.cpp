50b3e3a229226fb61a54528f82d71632808594f3
Pomarancz789 <granat-02krawcy@icloud.com>
2021-12-15 10:29:28 +0100

    Initial Commit

---
+++ sortowanie-przez-wybor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sortowanie-przez-wybor
+//
+//  Created by Jakub Wtorkiewicz on 15/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
